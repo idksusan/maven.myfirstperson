059688e083c100552b34644b5af36028098450b7
Susan <sttabassum93@gmail.com>
2019-12-18 14:41:24 -0500

    Initial push

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -4,13 +4,16 @@ package com.github.curriculeon;
  * Created by leon on 12/16/2019.
  */
 public class Person {
-    public Person(){}
+    String firstName;
+    String lastName;
 
-    public Person(String firstName, String lastName) {
+    public Person(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
-    public String getFirstName() {
-        return null;
+    public String getFirstName(String firstName) {
+        return firstName;
     }
 
     public void setFirstName(String firstName) {
